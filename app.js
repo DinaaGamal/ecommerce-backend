e343481e4602953a9d0a6f7a234e08c357f24c1b
dina <dina@gmail.com>
2019-04-13 22:12:53 +0200

    api/

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-
 const express = require('express');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const errorHandler = require('./handlers/error');
 const logger = require('morgan');
 const usersRouter = require('./routes/users');
 const productRouter = require('./routes/products');
-const indexRouter = require("./routes");
-const {Product} = require("./models/db");
+const indexRouter = require('./routes');
+const { Product } = require('./models/db');
 const app = express();
 const { loginRequired, ensureCorrectUser } = require('./middelware/auth');
 app.use(logger('dev'));
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 // // app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use("/", indexRouter) 
-app.use('/users', usersRouter);
-app.use('/users/:id/products', loginRequired, ensureCorrectUser, productRouter); 
+// app.use("/", indexRouter)
+app.use('/api/users', usersRouter);
+app.use('/api/users/:id/products', loginRequired, ensureCorrectUser, productRouter);
 
 app.get('/products', loginRequired, async (req, res, next) => {
-	try { 
+	try {
 		let products = await Product.find().sort({ createdAt: 'desc' }).populate('user', {
 			username: true,
-			email :true
+			email: true
 		});
 		return res.status(200).json(products);
 	} catch (error) {
 		return next(error);
 	}
-}); 
+});
 
 //error handler
 app.use((req, res, next) => {
@@ -38,6 +37,5 @@ app.use((req, res, next) => {
 });
 
 app.use(errorHandler);
-
 
 module.exports = app;
